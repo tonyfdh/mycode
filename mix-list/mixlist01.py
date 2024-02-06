2b6d218ac963e9af7c636fda9989130462e156ea
tonyfdh <anthony.hannasch@gmail.com>
2024-02-06 21:01:23 +0000

    Learning lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#set the list
+my_list = [ "192.168.0.5", 5060, "UP"]
+
+#print the first item
+print("The first item in the list (IP): " + my_list[0] )
+
+#print the second item
+print("The second item in the list (port): " + str(my_list[1]))
+
+#print the third item
+print("The last item in the list (state): " + my_list[2])
+
---
+++ mix-list/mixlist02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print("The IP addresses from the list are " + iplist[3] + " and " + iplist[4] + ".")
+
