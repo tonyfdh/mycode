89a3e4cf648a125b40c41cc0feff93c94306be28
tonyfdh <anthony.hannasch@gmail.com>
2024-02-09 23:19:14 +0000

    Beer challenge done

---
+++ challenges/beerloop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import time
+
+for beer in range(99,0,-1):
+    print(beer,"bottles of beer on the wall!")
+    print(beer,"bottles of beer! Take one down, pass it around")
+    time.sleep(.5)
+ 
+print("All outta beer, see ya later!")
